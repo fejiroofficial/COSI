c6f70256115db4cc6f8b5250d0fa803fb1ff642c
Fejiro Gospel <38490848+fejiroofficial@users.noreply.github.com>
2021-02-06 08:16:18 +0100

    Update information.js

--- src/redux/reducers/information.js
+++ src/redux/reducers/information.js
@@ -15,6 +15,7 @@ export default function information(state = initialState, action) {
       }
     case TOGGLE_PREVIEW:
       return {
+        ...state,
         showPreview: action.previewToogle,
       }
     default:
